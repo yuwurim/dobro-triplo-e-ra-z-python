4fbe14f602b8a23d67f58a1b53121def6965f909
Yuri Santos <96179643+yuwurim@users.noreply.github.com>
2023-03-22 07:58:28 -0300

    Add files via upload

---
+++ dob_triplo_raiz.py
@@ -0,0 +1,7 @@
+print('Dobro, triplo e raíz quadrada de um número.')
+
+n=int(input('Digite um número: '))
+d=n*2
+t=n*3
+r=n**(1/2)
+print('O dobro de {} é {}, o triplo é {} e a raíz quadrada é {}.'.format(n, d, t, r))
